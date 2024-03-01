989cac32f00d7a221f9902f67dc317181918441d
Rahul <Rahul@192.168.100.27>
2024-03-01 20:41:46 +0530

    rahul commit

---
+++ demo-xpath/src/main/java/com/rahul/shome/demo_xpath/rahul_shome.java
@@ -0,0 +1,13 @@
+package com.rahul.shome.demo_xpath;
+
+public class rahul_shome {
+
+	
+	public void sai() {
+		try {
+			System.out.println("heloo");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
