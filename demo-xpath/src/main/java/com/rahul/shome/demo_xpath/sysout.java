851046cc89b63c6dcd0a979058bb7d3945928a3a
Rahul <Rahul@192.168.100.27>
2024-03-01 16:29:08 +0530

    2d commit

---
+++ demo-xpath/src/main/java/com/rahul/shome/demo_xpath/sysout.java
@@ -0,0 +1,13 @@
+package com.rahul.shome.demo_xpath;
+
+public class sysout {
+	public  void pritsomething () {
+		try {
+			System.out.println("hi i am all");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+
+}
