b6d908e5a18dc19e92e75d78562dbf2bd20ab75c
Rahul <Rahul@192.168.100.27>
2024-03-01 16:38:15 +0530

    3rd commit prac

--- demo-xpath/src/main/java/com/rahul/shome/demo_xpath/rahul_oj.java
+++ demo-xpath/src/main/java/com/rahul/shome/demo_xpath/rahul_oj.java
@@ -1,9 +1,9 @@
 package com.rahul.shome.demo_xpath;
 
-public class sysout {
+public class rahul_oj {
 	public  void pritsomething () {
 		try {
-			System.out.println("hi i am all  about you and how are u");
+			System.out.println("hi i am all");
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
