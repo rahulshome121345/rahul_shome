14be845c8d6c1cf1fdf079c87f13bdbd97d15dc1
Rahul <Rahul@192.168.100.27>
2024-03-01 14:05:17 +0530

    initial commit

---
+++ demo-xpath/src/main/java/com/rahul/shome/demo_xpath/App.java
@@ -0,0 +1,18 @@
+package com.rahul.shome.demo_xpath;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public String Hello() {
+		return "Hello World";
+	}
+    public static void main( String[] args )
+    {
+    	App ob =new App();
+        System.out.println( ob.Hello() );
+    }
+}
