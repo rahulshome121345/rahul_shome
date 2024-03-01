087d8e6154daf5204696e1b1412a4a4316e9a76f
Rahul <Rahul@192.168.100.27>
2024-03-01 21:06:40 +0530

    commit one 121

---
+++ demo-xpath/src/main/java/com/rahul/shome/demo_xpath/rajaja_das.java
@@ -0,0 +1,14 @@
+package com.rahul.shome.demo_xpath;
+
+public class rajaja_das {
+	
+	
+	public void sai() {
+		try {
+			System.out.println("heloo");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
